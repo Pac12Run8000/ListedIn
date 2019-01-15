821a50cf1e85397899b76c0b18e7c1f4cf6a6dce
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2019-01-15 00:24:35 -0800

    feat: Add colors to an extensions file that contains all of the colors that will be used in the app.

--- ListedIn/Extensions/Extensions.swift
+++ ListedIn/Extensions/Extensions.swift
@@ -11,7 +11,27 @@ import UIKit
 
 extension UIColor {
     
+    static let brightGreen_1 = UIColor.colorFromHex("c9f658")
+    static let brightGreen_2 = UIColor.colorFromHex("dbff3d")
+    static let darkgreen = UIColor.colorFromHex("dbff3d")
+    static let greenCyan = UIColor.colorFromHex("8acbbb")
     
+    static func colorFromHex(_ hex:String) -> UIColor {
+        var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (hexString.hasPrefix("#")) {
+            hexString.remove(at: hexString.startIndex)
+        }
+        
+        if (hexString.count != 6) {
+            return UIColor.black
+        }
+        
+        var rgb:UInt32 = 0
+        Scanner(string: hexString).scanHexInt32(&rgb)
+        
+        return UIColor.init(red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0, green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0, blue: CGFloat(rgb & 0x0000FF) / 255.0, alpha: 1.0)
+    }
     
     
 }
