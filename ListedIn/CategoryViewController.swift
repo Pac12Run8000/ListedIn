1a0842708807a5efe26e3590f6fb60c048d07e8c
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2019-01-15 15:08:25 -0800

    feat: AlertViewController

--- ListedIn/CategoryViewController.swift
+++ ListedIn/CategoryViewController.swift
@@ -10,14 +10,54 @@ import UIKit
 
 class CategoryViewController: UIViewController {
     
+    var categoryTextField:UITextField?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       print("Category")
+    }
+    
+    @IBAction func addCategoryButtonAction(_ sender: Any) {
+        
+       
         
     }
+    
 
 
+}
+
+// MARK: This is the alertViewController functionality
+extension CategoryViewController {
+    
+    
+    func categoryAlertController() {
+        let alertDialog = UIAlertController(title: "", message: "Enter a category for your list.", preferredStyle: .alert)
+        
+        let cancelDialog = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let saveDialog = UIAlertAction(title: "Save", style: .default) { (action) in
+            
+            print("Category:\(String(describing: self.categoryTextField?.text))")
+            
+        }
+        saveDialog.isEnabled = false
+        alertDialog.addAction(saveDialog)
+        alertDialog.addAction(cancelDialog)
+        
+        
+        alertDialog.addTextField { (textField) in
+            NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: textField, queue: .main, using: { (notification) in
+                if let text = textField.text, !text.isEmpty, text != "" {
+                    saveDialog.isEnabled = true
+                } else {
+                    saveDialog.isEnabled = false
+                }
+            })
+            self.categoryTextField?.placeholder = "category"
+            self.categoryTextField = textField
+        }
+        present(alertDialog, animated: true, completion: nil)
+    }
+    
 }
 
