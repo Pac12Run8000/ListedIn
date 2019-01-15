fc3a7d375f811dde327f0e4881aee85f272ebd7e
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2019-01-15 13:17:34 -0800

    feat: Finished the basic layout

--- ListedIn/CategoryViewController.swift
+++ ListedIn/CategoryViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoryViewController: UIViewController {
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
+       print("Category")
         
     }
 
