cda60c0f227380d9c3e4fea8efb7c837a99d7cab
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2019-01-16 07:27:53 -0800

    feat: The add, edit and delete functionality for the CategoryViewController is completed.

--- ListedIn/CategoryViewController.swift
+++ ListedIn/CategoryViewController.swift
@@ -12,7 +12,7 @@ class CategoryViewController: UIViewController {
 
     var categoryTextField:UITextField?
     
-    var categories = [String]()
+    var categories = [Category]()
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -36,7 +36,7 @@ class CategoryViewController: UIViewController {
 }
 
 
-// MARK:- This is where you'll find all tableView and tableviewcell rendering functionality
+// MARK:- The tableView delegate functions
 extension CategoryViewController:UITableViewDataSource, UITableViewDelegate {
     
     func setDynamicRowHeight() {
@@ -50,6 +50,9 @@ extension CategoryViewController:UITableViewDataSource, UITableViewDelegate {
         tableView.dataSource = self
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
@@ -58,7 +61,7 @@ extension CategoryViewController:UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UITableViewCell
-        cell.textLabel?.text = categories[indexPath.row]
+        cell.textLabel?.text = categories[indexPath.row].name
         cell.detailTextLabel?.text = ""
         cell.backgroundColor = UIColor.greenCyan
         cell.textLabel?.textColor = UIColor.white
@@ -66,14 +69,70 @@ extension CategoryViewController:UITableViewDataSource, UITableViewDelegate {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let category = categories[indexPath.row]
+        let editAction = UITableViewRowAction(style: .default, title: "Edit") { (rowAction, indexPath) in
+            self.editCategory(category: category, indexPath: indexPath)
+        }
+        
+        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (rowAction, indexPath) in
+            self.deleteCategory(input: category, indexPath: indexPath)
+        }
+        editAction.backgroundColor = .blue
+        deleteAction.backgroundColor = .red
+        return [editAction, deleteAction]
+    }
     
+    
+}
+
+// MARK:- Adding, Updating and Deleting data from the Categories array
+extension CategoryViewController {
+    
+    private func editCategory(category:Category, indexPath:IndexPath) {
+        let alert = UIAlertController(title: "", message: "Update the category", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { (action) in
+            guard let textField = alert.textFields?.first else {
+                return
+            }
+            
+            guard let textToEdit = textField.text, textToEdit.count != 0 else {
+                return
+            }
+            
+            category.name = textToEdit
+            self.tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addTextField()
+        guard let textField = alert.textFields?.first else {
+            return
+        }
+        textField.placeholder = "Update category"
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func deleteCategory(input:Category, indexPath:IndexPath) {
+        self.categories.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+    
+    private func addCategoryToTableViewAndArray(catagory:Category)  {
+        self.categories.insert(catagory, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .left)
+    }
 }
 
 
 
 
 
-// MARK: This is where the alertController gets the category name that is to be passed to CoreData
+// MARK: AlertController functionality
 extension CategoryViewController {
     
     func categoryAlertController() {
@@ -82,8 +141,10 @@ extension CategoryViewController {
         let cancelDialog = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         let saveDialog = UIAlertAction(title: "Save", style: .default) { [weak self] action in
             
-            if let category = self?.categoryTextField?.text {
-                self?.addCategory(catagory: category)
+            if let categoryName = self?.categoryTextField?.text {
+                let myCat = Category()
+                myCat.name = categoryName
+                self?.addCategoryToTableViewAndArray(catagory: myCat)
             }
         }
         
@@ -107,11 +168,8 @@ extension CategoryViewController {
     }
  
     
-    func addCategory(catagory:String)  {
-        self.categories.append(catagory)
-        let indexPath = IndexPath(row: self.categories.count - 1, section: 0)
-        tableView.insertRows(at: [indexPath], with: .fade)
-    }
+    
     
 }
+
 
---
+++ ListedIn/Model/Category.swift
@@ -0,0 +1,20 @@
+//
+//  Category.swift
+//  ListedIn
+//
+//  Created by Michelle Grover on 1/16/19.
+//  Copyright © 2019 Norbert Grover. All rights reserved.
+//
+
+import Foundation
+
+class Category {
+    
+    var name:String?
+    
+    init() {
+        self.name = nil
+    }
+    
+    
+}
