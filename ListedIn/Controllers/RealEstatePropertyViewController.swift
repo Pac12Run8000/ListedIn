e04c1f6be1132491221039abe2effcc3925f4f37
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2019-01-19 10:09:59 -0800

    feat: Finished up the tableViewCell for the address.

--- ListedIn/Controllers/RealEstatePropertyViewController.swift
+++ ListedIn/Controllers/RealEstatePropertyViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class RealEstatePropertyViewController: UIViewController {
     
+    var addresses:[String]!
+    
     var dataController:DataController!
     var category:Category!
-
+    @IBOutlet weak var tableView: UITableView!
+   
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addresses = ["1810 San Jose Ave. Alameda CA 94501", "20284 Fenmore St, Detroit MI", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco", "laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."]
+        
+       setupTableViewProperties()
+        
         
         
     }
@@ -27,4 +38,42 @@ class RealEstatePropertyViewController: UIViewController {
 
    
 
+}
+
+
+
+// MARK:- TableViewDelegate and TableViewDataSource
+extension RealEstatePropertyViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return addresses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = addresses[indexPath.row]
+        cell.backgroundColor = UIColor.greenCyan
+        cell.textLabel?.textColor = UIColor.white
+        return cell
+    }
+    
+    
+    
+}
+
+// MARK:- TableView properties like cellsize, datasource, delegate, backgroundColor, etc
+extension RealEstatePropertyViewController {
+    
+    private func setupTableViewProperties() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.estimatedRowHeight = 90
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        tableView.separatorColor = UIColor.darkgreen
+        tableView.backgroundColor = UIColor.brightGreen_1
+    }
+    
 }
