3715bb1656ab8c3f845c09b3187ecdc9608574c9
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2019-01-20 10:43:33 -0800

    feat: finished a little preliminary functionality for the searchBar

--- ListedIn/Controllers/addAddressController.swift
+++ ListedIn/Controllers/addAddressController.swift
@@ -22,9 +22,11 @@ class addAddressController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        addresses = ["San Antonio TX", "Houston TX", "Dallas TX", "Austin TX", "New Haven CT", "New York, NY", "Detroit, MI", "Chicago IL"]
+        addresses = ["Current Location", "San Antonio TX", "Houston TX", "Dallas TX", "Austin TX", "New Haven CT", "New York, NY", "Detroit, MI", "Chicago IL"]
         
         view.backgroundColor = UIColor.darkgreen
+        
+        
         searchBarOutlet.delegate = self
         
         tableView.delegate = self
@@ -39,8 +41,9 @@ class addAddressController: UIViewController {
 
 }
 
-
+// MARK: UITableViewDelegate methods
 extension addAddressController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if isSearching {
             return addressPredictions.count
@@ -55,17 +58,28 @@ extension addAddressController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         if isSearching {
             cell.textLabel?.text = addressPredictions[indexPath.row]
+            cell.textLabel?.textColor = addressPredictions[indexPath.row] == "Current Location" ? UIColor.greenCyan : UIColor.black
         } else {
             cell.textLabel?.text = addresses[indexPath.row]
+            cell.textLabel?.textColor = addresses[indexPath.row] == "Current Location" ? UIColor.greenCyan : UIColor.black
         }
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if isSearching {
+            searchBarOutlet.text = addressPredictions[indexPath.row]
+        } else {
+            searchBarOutlet.text = addresses[indexPath.row]
+        }
     }
     
     
 }
 
 
-// MARK:- SearchBar Delegate
+// MARK:- SearchBar Delegate Methods
 extension addAddressController: UISearchBarDelegate {
     
     
@@ -76,9 +90,17 @@ extension addAddressController: UISearchBarDelegate {
         }
     }
     
+    
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         addressPredictions = addresses.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
         isSearching = true
         tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        if searchBar.text == "" {
+            searchBar.text = "Current Location"
+        }
     }
 }
